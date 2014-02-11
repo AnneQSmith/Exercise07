7491d66f2178b87a1b5df787978f7adbe7d0856d
Hackbright Student <info@hackbrightacademy.com>
2014-02-11 12:28:54 -0800

    Working version

--- exercise07.py
+++ exercise07.py
@@ -1,7 +1,36 @@
+#Exercise 7
+#In this directory, you will find a text file, scores.txt, containing a series of local restaurant 
+#ratings. Each line looks like this:
+
+#Restaurant Name:Rating
+# out the ratings in alphabetical order by restaurant
+
+
+
+#TODOs:
+#Open the file
+
 scores = open('scores.txt')
-score_dict = {}
-temp_list = []
+#
+# Read entries line by line; assign to dictionary as key value pairs
+# 
+# Restaurante name will be keys, rating is value
+# use some sorting method to sort
+#print
+
+rest_dict = {}
 for line in scores:
-    temp_list = line.split(':')
-    temp_list[1].rstrip('\n')
-    if score_dict.get([temp_list[0]])
+#split each line by ":"
+
+    line_tokens = line.split(':')
+
+#create dictionary entry for each restaurant, assign a rating value
+
+    rest_dict[line_tokens[0]]= int(line_tokens[1])
+ 
+    sorted_keylist = sorted(rest_dict.keys())
+
+#print sorted_keylist
+
+for restaurant in sorted_keylist:
+    print"%s gets %d stars!" % (restaurant, rest_dict[restaurant])
